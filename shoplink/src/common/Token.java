b19c524f7f7d7e30866f0fc1e5edc1ac39d899de
tanakamasato <tanakamasato@192.168.3.14>
2020-11-21 14:55:45 +0900

    Token制作、web.xml編集

---
+++ shoplink/src/common/Token.java
@@ -0,0 +1,66 @@
+package common;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class Token
+ */
+@WebFilter("/Token")
+public class Token implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+    HttpServletRequest req = (HttpServletRequest)request;
+    if(req.getMethod().equals("GET")) {
+      getToken(req);
+    } else {
+      if(!checkToken(req)) {
+        throw new ServletException("不正なアクセスが行われました");
+      }
+    }
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+  public void getToken(HttpServletRequest request) {
+    byte token[] = new byte[16];
+    StringBuffer buff = new StringBuffer();
+    SecureRandom random = null;
+    try {
+      random = SecureRandom.getInstance("SHA1PRNG");
+      random.nextBytes(token);
+      for(int i = 0; i < 16; i++) {
+        buff.append(String.format("%02x", token[i]));
+        HttpSession session = request.getSession();
+        session.setAttribute("token", buff.toString());
+      }
+    } catch(NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private boolean checkToken(HttpServletRequest request) {
+    HttpSession session = request.getSession();
+    String s_token = (String)session.getAttribute("token");
+    String r_token = request.getParameter("token");
+    if(s_token == null || r_token == null || r_token.isEmpty()) {
+      return false;
+    }
+    return s_token.equals(r_token);
+  }
+
+}
