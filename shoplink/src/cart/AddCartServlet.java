684fd98af5e85167082da3ca9fe6c0c2d89f8119
tanakamasato <tanakamasato@192.168.3.14>
2020-12-08 17:39:19 +0900

    add

--- shoplink/src/cart/AddCartServlet.java
+++ shoplink/src/cart/AddCartServlet.java
@@ -14,59 +14,66 @@ import javax.servlet.http.HttpSession;
  * Servlet implementation class AddCartServlet
  */
 @WebServlet("/AddCartServlet")
-public class AddCartServlet extends HttpServlet {
+public class AddCartServlet extends HttpServlet
+{
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	  ProductDTO product = new ProductDTO();
-    HttpSession session = request.getSession();
-	  CartDTO cart = (CartDTO) session.getAttribute("cart");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        // TODO Auto-generated method stub
+        ProductDTO product = new ProductDTO();
+        HttpSession session = request.getSession();
+        CartDTO cart = (CartDTO) session.getAttribute("cart");
 
-	  if(cart == null) {
-	    cart = new CartDTO();
-	    product.setId(Integer.parseInt(request.getParameter("id")));
-	    product.setTitle(request.getParameter("title"));
-	    product.setArtist(request.getParameter("artist"));
-	    product.setPrice(Integer.parseInt(request.getParameter("price")));
-	    product.setQuantity(Integer.parseInt(request.getParameter("quantity")));
-	    product.setImages(request.getParameter("images").replace(".jpg", ""));
+        if (cart == null)
+        {
+            cart = new CartDTO();
+            product.setId(Integer.parseInt(request.getParameter("id")));
+            product.setTitle(request.getParameter("title"));
+            product.setArtist(request.getParameter("artist"));
+            product.setPrice(Integer.parseInt(request.getParameter("price")));
+            product.setQuantity(Integer.parseInt(request.getParameter("quantity")));
+            product.setImages(request.getParameter("images").replace(".jpg", ""));
 
-	  } else {
-	  Iterator<ProductDTO> itr_cart = cart.getProduct().iterator();
-	  boolean foundProduct = false;
-	  while(itr_cart.hasNext()) {
-	    ProductDTO dto = itr_cart.next();
-	    if(dto.getId() == Integer.parseInt(request.getParameter("id"))) {
-	      product.setId(Integer.parseInt(request.getParameter("id")));
-	      product.setTitle(request.getParameter("title"));
-	      product.setArtist(request.getParameter("artist"));
-	      product.setPrice(Integer.parseInt(request.getParameter("price")));
-	      product.setQuantity(Integer.parseInt(request.getParameter("quantity")) + dto.getQuantity());
-	      product.setImages(request.getParameter("images").replace(".jpg", ""));
-	      itr_cart.remove();
-	      foundProduct = true;
-	      break;
-	    }
-	  }
-	  if(!foundProduct) {
-      product.setId(Integer.parseInt(request.getParameter("id")));
-      product.setTitle(request.getParameter("title"));
-      product.setArtist(request.getParameter("artist"));
-      product.setPrice(Integer.parseInt(request.getParameter("price")));
-      product.setQuantity(Integer.parseInt(request.getParameter("quantity")));
-      product.setImages(request.getParameter("images").replace(".jpg", ""));
-	  }
-   }
+        }
+        else
+        {
+            Iterator<ProductDTO> itr_cart = cart.getProduct().iterator();
+            boolean foundProduct = false;
+            while (itr_cart.hasNext())
+            {
+                ProductDTO dto = itr_cart.next();
+                if (dto.getId() == Integer.parseInt(request.getParameter("id")))
+                {
+                    product.setId(Integer.parseInt(request.getParameter("id")));
+                    product.setTitle(request.getParameter("title"));
+                    product.setArtist(request.getParameter("artist"));
+                    product.setPrice(Integer.parseInt(request.getParameter("price")));
+                    product.setQuantity(Integer.parseInt(request.getParameter("quantity")) + dto.getQuantity());
+                    product.setImages(request.getParameter("images").replace(".jpg", ""));
+                    itr_cart.remove();
+                    foundProduct = true;
+                    break;
+                }
+            }
+            if (!foundProduct)
+            {
+                product.setId(Integer.parseInt(request.getParameter("id")));
+                product.setTitle(request.getParameter("title"));
+                product.setArtist(request.getParameter("artist"));
+                product.setPrice(Integer.parseInt(request.getParameter("price")));
+                product.setQuantity(Integer.parseInt(request.getParameter("quantity")));
+                product.setImages(request.getParameter("images").replace(".jpg", ""));
+            }
+        }
 
-	  cart.getProduct().add(product);
-	  CartLogic logic = new CartLogic();
-	  TotalAmountDTO totalAmount = logic.execute(cart);
+        cart.getProduct().add(product);
+        CartLogic logic = new CartLogic();
+        TotalAmountDTO totalAmount = logic.execute(cart);
 
-    session.setAttribute("cart", cart);
-    session.setAttribute("totalAmount", totalAmount);
-    request.setAttribute("msg", product.getTitle() + "をカートに追加しました");
-    request.getRequestDispatcher("shoppingCart.jsp").forward(request, response);
+        session.setAttribute("cart", cart);
+        session.setAttribute("totalAmount", totalAmount);
+        request.setAttribute("msg", product.getTitle() + "をカートに追加しました");
+        request.getRequestDispatcher("shoppingCart.jsp").forward(request, response);
 
-
-	}
+    }
 }
--- shoplink/src/cart/CartLogic.java
+++ shoplink/src/cart/CartLogic.java
@@ -2,7 +2,8 @@ package cart;
 
 import java.util.List;
 
-public class CartLogic {
+public class CartLogic
+{
 
     private double tax;
     private int taxAmount;
@@ -10,30 +11,32 @@ public class CartLogic {
     private int total;
     private int shipping;
 
-    public CartLogic() {
-      this.tax = 0.08;
-      this.taxAmount = 0;
-      this.subTotal = 0;
-      this.shipping = 2000;
-      this.total = 0;
+    public CartLogic()
+    {
+        this.tax = 0.08;
+        this.taxAmount = 0;
+        this.subTotal = 0;
+        this.shipping = 2000;
+        this.total = 0;
     }
 
+    public TotalAmountDTO execute(CartDTO cart)
+    {
+        List<ProductDTO> products = cart.getProduct();
+        for (ProductDTO dto : products)
+        {
+            this.subTotal = dto.getPrice() * dto.getQuantity() + this.subTotal;
+        }
+        this.taxAmount = (int) (this.subTotal * this.tax);
+        this.total = this.subTotal + this.taxAmount + this.shipping;
 
-  public TotalAmountDTO execute(CartDTO cart) {
-   List <ProductDTO> products = cart.getProduct();
-   for(ProductDTO dto : products) {
-     this.subTotal = dto.getPrice() * dto.getQuantity() + this.subTotal;
-   }
-   this.taxAmount = (int)(this.subTotal * this.tax);
-   this.total = this.subTotal + this.taxAmount + this.shipping;
+        TotalAmountDTO totalAmount = new TotalAmountDTO();
+        totalAmount.setSubTotal(this.subTotal);
+        totalAmount.setTaxAmount(this.taxAmount);
+        totalAmount.setShipping(this.shipping);
+        totalAmount.setTotalAmount(this.total);
 
-   TotalAmountDTO totalAmount = new TotalAmountDTO();
-   totalAmount.setSubTotal(this.subTotal);
-   totalAmount.setTaxAmount(this.taxAmount);
-   totalAmount.setShipping(this.shipping);
-   totalAmount.setTotalAmount(this.total);
+        return totalAmount;
 
-   return totalAmount;
-
-  }
+    }
 }
